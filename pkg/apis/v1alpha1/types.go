db5d19c90b37d24dc139e59bb47b8e2238b47b0d
joshvanl <vleeuwenjoshua@gmail.com>
2021-08-10 15:50:18 +0100

    Removes unused fields in v1alpha1:
    Signed-off-by: joshvanl <vleeuwenjoshua@gmail.com>

--- pkg/apis/v1alpha1/types.go
+++ pkg/apis/v1alpha1/types.go
@@ -17,46 +17,22 @@ limitations under the License.
 package v1alpha1
 
 const (
-	MetaDataFileName = "metadata.json"
+	IssuerNameKey  = "csi.cert-manager.io/issuer-name"
+	IssuerKindKey  = "csi.cert-manager.io/issuer-kind"
+	IssuerGroupKey = "csi.cert-manager.io/issuer-group"
+
+	CommonNameKey = "csi.cert-manager.io/common-name"
+	DNSNamesKey   = "csi.cert-manager.io/dns-names"
+	IPSANsKey     = "csi.cert-manager.io/ip-sans"
+	URISANsKey    = "csi.cert-manager.io/uri-sans"
+	DurationKey   = "csi.cert-manager.io/duration"
+	IsCAKey       = "csi.cert-manager.io/is-ca"
+	KeyUsagesKey  = "csi.cert-manager.io/key-usages"
+
+	CAFileKey   = "csi.cert-manager.io/ca-file"
+	CertFileKey = "csi.cert-manager.io/certificate-file"
+	KeyFileKey  = "csi.cert-manager.io/privatekey-file"
+
+	RenewBeforeKey  = "csi.cert-manager.io/renew-before"
+	ReusePrivateKey = "csi.cert-manager.io/reuse-private-key"
 )
-
-const (
-	CSIPodNameKey      = "csi.storage.k8s.io/pod.name"
-	CSIPodNamespaceKey = "csi.storage.k8s.io/pod.namespace"
-	CSIPodUIDKey       = "csi.storage.k8s.io/pod.uid"
-	CSIEphemeralKey    = "csi.storage.k8s.io/ephemeral"
-)
-
-const (
-	IssuerNameKey  string = "csi.cert-manager.io/issuer-name"
-	IssuerKindKey  string = "csi.cert-manager.io/issuer-kind"
-	IssuerGroupKey string = "csi.cert-manager.io/issuer-group"
-
-	CommonNameKey string = "csi.cert-manager.io/common-name"
-	DNSNamesKey   string = "csi.cert-manager.io/dns-names"
-	IPSANsKey     string = "csi.cert-manager.io/ip-sans"
-	URISANsKey    string = "csi.cert-manager.io/uri-sans"
-	DurationKey   string = "csi.cert-manager.io/duration"
-	IsCAKey       string = "csi.cert-manager.io/is-ca"
-	KeyUsagesKey  string = "csi.cert-manager.io/key-usages"
-
-	CAFileKey   string = "csi.cert-manager.io/ca-file"
-	CertFileKey string = "csi.cert-manager.io/certificate-file"
-	KeyFileKey  string = "csi.cert-manager.io/privatekey-file"
-
-	RenewBeforeKey  string = "csi.cert-manager.io/renew-before"
-	ReusePrivateKey string = "csi.cert-manager.io/reuse-private-key"
-)
-
-type MetaData struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
-	Size int64  `json:"size"`
-
-	// real file path in the host file system
-	Path string `json:"path"`
-	// target path to mount to
-	TargetPath string `json:"targetPath"`
-
-	Attributes map[string]string `json:"attributes"`
-}
