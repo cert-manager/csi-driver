c233d94d70fc4c2c7e2f6ad04d5f4fc8132fba17
joshvanl <vleeuwenjoshua@gmail.com>
2021-08-10 17:08:58 +0100

    Updates boilerplate header files everywhere
    Signed-off-by: joshvanl <vleeuwenjoshua@gmail.com>

--- test/e2e/util/csr.go
+++ test/e2e/util/csr.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2019 The Jetstack cert-manager contributors.
+Copyright 2021 The cert-manager Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -28,23 +28,6 @@ import (
 	csiapi "github.com/jetstack/cert-manager-csi/pkg/apis/v1alpha1"
 )
 
-func RenewTimeFromNotAfter(notBefore time.Time, notAfter time.Time, renewBeforeString string) (time.Duration, error) {
-	renewBefore, err := time.ParseDuration(renewBeforeString)
-	if err != nil {
-		return 0, fmt.Errorf("failed to parse renew before: %s", err)
-	}
-
-	validity := notAfter.Sub(notBefore)
-	if renewBefore > validity {
-		return 0, fmt.Errorf("renewal duration is longer than certificate validity: %s %s",
-			renewBefore, validity)
-	}
-
-	dur := notAfter.Add(-renewBefore).Sub(time.Now())
-
-	return dur, nil
-}
-
 func CertificateRequestReady(cr *cmapi.CertificateRequest) bool {
 	readyType := cmapi.CertificateRequestConditionReady
 	readyStatus := cmmeta.ConditionTrue
@@ -57,44 +40,4 @@ func CertificateRequestReady(cr *cmapi.CertificateRequest) bool {
 	}
 
 	return false
-}
-
-func KeyUsagesFromAttributes(attr map[string]string) []cmapi.KeyUsage {
-	usageCSV := attr[csiapi.KeyUsagesKey]
-
-	if len(usageCSV) == 0 {
-		return nil
-	}
-
-	var keyUsages []cmapi.KeyUsage
-	for _, usage := range strings.Split(usageCSV, ",") {
-		keyUsages = append(keyUsages, cmapi.KeyUsage(strings.TrimSpace(usage)))
-	}
-
-	return keyUsages
-}
-
-func keyUsagesMatch(a, b []cmapi.KeyUsage) bool {
-	if len(a) != len(b) {
-		return false
-	}
-
-	aa, bb := make([]cmapi.KeyUsage, len(a)), make([]cmapi.KeyUsage, len(b))
-	copy(aa, a)
-	copy(bb, b)
-
-	sort.SliceStable(aa, func(i, j int) bool {
-		return aa[i] < aa[j]
-	})
-	sort.SliceStable(bb, func(i, j int) bool {
-		return bb[i] < bb[j]
-	})
-
-	for i, s := range aa {
-		if s != bb[i] {
-			return false
-		}
-	}
-
-	return true
 }
