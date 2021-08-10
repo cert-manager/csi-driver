23100224127273a0a3b4bc98fb024b69bd04e96d
joshvanl <vleeuwenjoshua@gmail.com>
2021-08-10 16:29:08 +0100

    Improve options structure, adds comments, logging with names
    Signed-off-by: joshvanl <vleeuwenjoshua@gmail.com>

--- cmd/app/options/options.go
+++ cmd/app/options/options.go
@@ -132,18 +132,13 @@ func (o *Options) addAppFlags(fs *pflag.FlagSet) {
 		"log-level", "v", "1",
 		"Log level (1-5).")
 
-	fs.StringVar(&o.NodeID, "node-id", "",
-		"The name of the node which is hosting this driver instance.")
+	fs.StringVar(&o.NodeID, "node-id", "", "The name of the node which is hosting this driver instance.")
 
-	fs.StringVar(&o.Endpoint, "endpoint", "",
-		"The endpoint that the driver will connect to the Kubelet.")
+	fs.StringVar(&o.Endpoint, "endpoint", "", "The endpoint that the driver will connect to the Kubelet.")
 
-	fs.StringVar(&o.DriverName, "driver-name", "csi.cert-manager.io",
-		"The name of this CSI driver which will be shared with the Kubelet.")
+	fs.StringVar(&o.DriverName, "driver-name",
+		"csi.cert-manager.io", "The name of this CSI driver which will be shared with the Kubelet.")
 
-	fs.StringVar(&o.DataRoot, "data-root", "/csi-data-dir",
-		"The directory that the driver will write and mount volumes from.")
-
-	fs.BoolVar(&o.UseRequestToken, "use-request-token", false,
-		"Use the empty audience request token for creating CertificateRequests. Requires the request token to be defined on the CSIDriver manifest.")
+	fs.StringVar(&o.DataRoot, "data-root",
+		"/csi-data-dir", "The directory that the driver will write and mount volumes from.")
 }
