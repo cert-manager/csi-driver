f44a43ef226c15e3bc725717867580f0f5caded7
Tim Ramlot <42113979+inteon@users.noreply.github.com>
2023-05-24 19:34:40 +0200

    replace github.com/cert-manager/cert-manager/test/e2e/framework/log with github.com/cert-manager/csi-driver/test/e2e/framework/log
    Signed-off-by: Tim Ramlot <42113979+inteon@users.noreply.github.com>

--- test/e2e/framework/helper/certificaterequest.go
+++ test/e2e/framework/helper/certificaterequest.go
@@ -19,19 +19,19 @@ package helper
 import (
 	"context"
 	"fmt"
-	"k8s.io/apimachinery/pkg/types"
 	"time"
 
 	corev1 "k8s.io/api/core/v1"
 	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/wait"
 
 	apiutil "github.com/cert-manager/cert-manager/pkg/api/util"
 	cmapi "github.com/cert-manager/cert-manager/pkg/apis/certmanager/v1"
 	cmmeta "github.com/cert-manager/cert-manager/pkg/apis/meta/v1"
-	"github.com/cert-manager/cert-manager/test/e2e/framework/log"
 
+	"github.com/cert-manager/csi-driver/test/e2e/framework/log"
 	"github.com/cert-manager/csi-driver/test/e2e/util"
 )
 
